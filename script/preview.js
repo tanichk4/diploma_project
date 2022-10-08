1d1a4963521dc5a31bc70c8a51cdd154692d9c67
Tetiana Fedorkiv <110106629+tanichk4@users.noreply.github.com>
2022-10-08 15:46:10 +0300

    Add files via upload

--- script/preview.js
+++ script/preview.js
@@ -2,5 +2,7 @@
 $('.preview-items > div').on('click', function(){
     $(this).next('.preview-text_small').slideToggle(300)
     $(this).children('.preview-h3_small').toggleClass('preview-h3_small-change')
-    $(this).children('.preview-br').toggleClass('preview-br-change')
+    // $(this).children('.preview-br').toggleClass('preview-br-change')
+    // $(this).next('.preview-flex-small').toggleClass('toggle-hr-prev')
+    $(this).parent('.preview-items').toggleClass('toggle-hr-prev')
 })
