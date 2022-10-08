eb101a985cc3274de2473a9fa7623435b80c3542
Tetiana Fedorkiv <110106629+tanichk4@users.noreply.github.com>
2022-10-08 09:59:47 +0300

    Add files via upload

--- script/preview.js
+++ script/preview.js
@@ -1,5 +1,6 @@
 
 $('.preview-items > div').on('click', function(){
     $(this).next('.preview-text_small').slideToggle(300)
-    // $(this).next('.about-text').slideToggle(300)
+    $(this).children('.preview-h3_small').toggleClass('preview-h3_small-change')
+    $(this).children('.preview-br').toggleClass('preview-br-change')
 })
