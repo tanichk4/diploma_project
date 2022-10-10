5a889d81cb03f7d3498e423416405f7271453bb2
Tetiana Fedorkiv <110106629+tanichk4@users.noreply.github.com>
2022-10-10 17:48:56 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 $('.about-items > div').on('click', function(){
     $(this).children('.about-icon').toggleClass('about-rotate-icon')
     $(this).parent('.about-items').toggleClass('change-color')
+    $(this).children('.about-location').toggleClass('change-text')
     $(this).next('.about-text').slideToggle(300)
 })
